33138a3513f6d6c755bd099eb08e98e47f9b5c54
jameslin12357 <jameslin12357@outlook.com>
2020-05-08 12:58:15 +0800

    cm#6

--- index.js
+++ index.js
@@ -4,8 +4,17 @@ var child1 = document.getElementById('child1');
 var child2 = document.getElementById('child2');
 
 var child3 = document.getElementById('child3');
-
+var buttonOptions = document.getElementsByClassName('buttonOptions');
 buttonIntro.addEventListener('click',function(e){
     child1.classList.add('hidden');
     child2.classList.remove('hidden');
 })
+
+for (let i = 0; i < buttonOptions.length; i++){
+    buttonOptions[i].addEventListener('drag',function(e){
+        e.target.classList.add('active');
+    });
+    buttonOptions[i].addEventListener('dragend',function(e){
+        e.target.classList.remove('active');
+    });
+}
