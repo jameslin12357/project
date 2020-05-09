5d9374a0599b7ca0bfe3c4a1b18c1cd505ad6569
jameslin12357 <jameslin12357@outlook.com>
2020-05-09 09:56:27 +0800

    cm#11

--- index.js
+++ index.js
@@ -2,16 +2,20 @@ var buttonIntro = document.getElementById('buttonIntro');
 var parent = document.getElementById('parent');
 var child1 = document.getElementById('child1');
 var child2 = document.getElementById('child2');
-var dragged = '', entered = '', c1,c2;
 var child3 = document.getElementById('child3');
-var buttonOptions = document.getElementsByClassName('buttonOptions');
-var buttonContainer = document.getElementById('buttonContainer');
+var buttonSubmit = document.getElementById('buttonSubmit');
+
 buttonIntro.addEventListener('click',function(e){
     child1.classList.add('hidden');
     child2.classList.remove('hidden');
-})
+});
+
+buttonSubmit.addEventListener('click',function(e){
+    child2.classList.add('hidden');
+    child3.classList.remove('hidden');
+});
 
 Sortable.create(list, {
     animation: 200,
-    ghostClass: "sortable-ghost"
+    ghostClass: "sortable"
 });
