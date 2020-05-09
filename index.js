1127593688912d473aa663dc0b876bcf41b814c1
jameslin12357 <jameslin12357@outlook.com>
2020-05-09 09:02:47 +0800

    cm#8

--- index.js
+++ index.js
@@ -2,19 +2,14 @@ var buttonIntro = document.getElementById('buttonIntro');
 var parent = document.getElementById('parent');
 var child1 = document.getElementById('child1');
 var child2 = document.getElementById('child2');
-
+var dragged = '', entered = '', c1,c2;
 var child3 = document.getElementById('child3');
 var buttonOptions = document.getElementsByClassName('buttonOptions');
+var buttonContainer = document.getElementById('buttonContainer');
 buttonIntro.addEventListener('click',function(e){
     child1.classList.add('hidden');
     child2.classList.remove('hidden');
 })
 
-for (let i = 0; i < buttonOptions.length; i++){
-    buttonOptions[i].addEventListener('drag',function(e){
-        e.target.classList.add('active');
-    });
-    buttonOptions[i].addEventListener('dragend',function(e){
-        e.target.classList.remove('active');
-    });
-}
+var el = document.getElementById('buttonContainer');
+var sortable = Sortable.create(el);
